8f33cb767139155e84c64403912d03f21ff7c675
Jan Machacek <jan.machacek@gmail.com>
2016-03-14 10:45:42 +0000

    Comments

--- profile-impl/src/main/java/fitness/muvr/profile/impl/UserState.java
+++ profile-impl/src/main/java/fitness/muvr/profile/impl/UserState.java
@@ -13,7 +13,6 @@ import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
-import java.util.Optional;
 import java.util.UUID;
 
 /**
